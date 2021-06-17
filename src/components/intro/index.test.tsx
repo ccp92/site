7a683181266f9ed6f5389b2fabb2d49532780bd1
Chris Parsons <mail@chrisparsons.dev>
2021-06-17 01:31:28 +0100

    feat: add contact me button to home page

--- src/components/intro/index.test.tsx
+++ src/components/intro/index.test.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Intro from ".";
-import { act } from "react-dom/test-utils";
 
 describe("Intro", () => {
   it("renders the link to github", () => {
     const wrapper = shallow(<Intro />);
-    const actionButton = wrapper.find({ "data-test": "action-button" });
+    const actionButton = wrapper.find({ "data-test": "main-action-button" });
     expect(actionButton.length).toBe(1);
     expect(actionButton.text()).toBe("Github profile");
+  });
+
+  it("renders the link to contact me", () => {
+    const wrapper = shallow(<Intro />);
+    const actionButton = wrapper.find({ "data-test": "action-button" });
+    expect(actionButton.length).toBe(1);
+    expect(actionButton.text()).toBe("Contact Me");
   });
 });
--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -2,13 +2,26 @@ const Intro = (): JSX.Element => {
   return (
     <div className="intro">
       <div>
-        <h1 className='header-text'>
+        <h1 className="header-text">
           Hey there, I'm <span className="dev-name">Chris Parsons</span>, a
           Software Engineer.
         </h1>
-        <a className="action-button" data-test="action-button" href="https://github.com/ccp92">
-          Github profile
-        </a>
+        <div className="button-row">
+          <a
+            className="action-button main-action-button"
+            data-test="main-action-button"
+            href="https://github.com/ccp92"
+          >
+            Github profile
+          </a>
+          <a
+            className="action-button"
+            data-test="action-button"
+            href={`mailto:${process.env.REACT_APP_EMAIL_ADDRESS}`}
+          >
+            Contact Me
+          </a>
+        </div>
       </div>
     </div>
   );
